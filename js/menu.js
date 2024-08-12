1d7fa91bb8f1336ffe1c473704fc876079fe14f0
jose.gomez <javierrumo2@gmail.com>
2024-08-12 12:57:51 +0200

    arreglo menu

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,20 @@
-// Obtener el botón y el menú
+// Obtener el botón, el menú y el logo
 const boton = document.getElementById('mostrar');
 const menu = document.getElementById('menu');
-const logo = document.getElementById('logo')
-// Agregar un evento clic al botón
+const logo = document.getElementById('logo');
 
+// Agregar un evento clic al botón
 boton.addEventListener('click', () => {
-// Si el menú está oculto, mostrarlo; si está visible, ocultarlo
-if (menu.style.display === 'none') {
-    menu.style.display = 'flex';
-    logo.style.display = 'none';
-} else {
-    menu.style.display = 'none';
-    logo.style.display = 'block';
-}
+    // Obtener el estilo de visualización actual del menú
+    const displayStyle = window.getComputedStyle(menu).display;
+
+    // Alternar entre mostrar y ocultar el menú
+    if (displayStyle === 'none') {
+        menu.style.display = 'flex';
+        logo.style.display = 'none';
+    } else {
+        menu.style.display = 'none';
+        logo.style.display = 'block';
+    }
 });
+
